cbb2e3215cd97fb9635052af629868c39ee44f1e
Mohamed Shemy <msh.work@outlook.sa>
2021-08-12 03:53:57 +0200

    Setup project structure

---
+++ COVID19/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  COVID19
+//
+//  Created by Mohamed Shemy on Thu 12 Aug 2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+}
+
